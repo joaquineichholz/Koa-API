0629f0d733e4916e30fc9d71ad71d911e4913d01
joaquineichholz <jeichholz3@uc.cl>
2020-04-28 09:49:28 -0400

    crud burger

--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,6 @@ const router = new KoaRouter();
 router.use('/', index.routes());
 console.log('\n\n\n ---------------------\n\n\t\t use /hamburguesa \n\n-------------------\n\n\n\n');
 router.use('/hamburguesa', hamburguesa.routes());
+router.use('/hamburguesa/id', hamburguesa.routes());
 
 module.exports = router;
--- src/routes/hamburguesa.js
+++ src/routes/hamburguesa.js
@@ -19,19 +19,21 @@ router.get('hamburguesa', '/', async (ctx) => {
  });
 
  // GET ONE Hamburger with an id
- router.get('hamburguesa/:id', '/', async (ctx) => {
+ router.get('/hamburguesa', '/:id', async (ctx) => {
   console.log('GET ONE')
-    if (!ctx.request.body.id) {
-        res.status(400).send({
-          message: "id invalido"
-        });
-        return;
-      }
 
-    const id_ = ctx.request.body.id;
-  
-    const hamburguesa = await ctx.orm.hamburguesa.findByPk({
-      where: {id: id_},
+  function isInteger(x) { return (Number.isInteger(Number(x))); }
+
+  const id_ = ctx.params.id;
+
+    if (!isInteger(id_)) {
+        ctx.response.body = "id invalido";
+        ctx.response.status = 400;
+        console.log('id integer:', isInteger(id_))
+        return
+      };
+ 
+    const hamburguesa = await ctx.orm.hamburguesa.findByPk(id_,{
       attributes: ['id', 'nombre', 'precio', 'descripcion', 'imagen']
     });
 
@@ -46,64 +48,6 @@ router.get('hamburguesa', '/', async (ctx) => {
         ctx.response.status = 404;
       }
     })
-      /*.catch(err => {
-        console.log("404 error")
-        res.status(404).send({
-          message: "hamburguesa inexistente" // revisar si es un error no encontrar la hamburguesa 
-        });
-      });*/
-
-// POST new Hamburger
-router.post('burgers', '/', async (ctx) => {
-  // Validate request
-  /* if (!req.body.title) {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
-      return;
-    }
-  */
-    // Create a Hamburger
-    console.log("crear hamburguesa")
-    const hamburguesas = await ctx.orm.hamburguesa.findAll({
-      attributes:  [Sequelize.fn('max', Sequelize.col('id'))],
-      raw: true,
-     });
-    console.log('hamburguesas:',hamburguesas[0].max)
-
-    var id_ = hamburguesas[0].max;
-    if (!id_){
-      console.log(' id:', id_)
-      id_ = 1;
-
-     // console.log('max_id', max_id)
-
-    }
-    else {
-      id_ += 1; 
-      console.log('else', id_)
-
-    }
-    console.log('fuera else', id_)
-
-    const hamburger = {
-      id:id_,
-      nombre: ctx.request.body.nombre,
-      descripcion: ctx.request.body.descripcion,
-      precio: ctx.request.body.precio,
-      imagen: ctx.request.body.imagen
-    };
-
-    
-    console.log("parametros: ", hamburger)
-  
-    // Save Hamburger in the database
-    const new_burger = ctx.orm.hamburguesa.build(hamburger);
-    new_burger.save({fields: ['id', 'nombre', 'precio', 'descripcion', 'foto']})
-    ctx.response.body = new_burger;
-    ctx.response.status = 201;
-
-  });
 
 // POST new hamburguesa
 router.post('hamburguesa', '/', async (ctx) => {
@@ -135,25 +79,35 @@ router.post('hamburguesa', '/', async (ctx) => {
 
 module.exports = router;
 
-/*
-router.get('hamburguesas', '/', async (ctx) => {
-  ctx.orm.hamburguesa.findAll(req, res);
+ // GET ONE Hamburger with an id
+ router.del('/hamburguesa', '/:id', async (ctx) => {
+  console.log('DELETE ONE')
 
-  .then(data => {
-    console.log("200")
-    res.status(200).send(data);
-  })
-  .catch(err => {
-    console.log("500 error -- :(")
-    res.status(500).send({
-      message:
-        err.message || "Some error occurred while retrieving hamburguesas."
+  function isInteger(x) { return (Number.isInteger(Number(x))); }
+
+  const id_ = ctx.params.id;
+
+    if (!isInteger(id_)) {
+        ctx.response.body = "hamburguesa inexistente";
+        ctx.response.status = 404;
+        console.log('id integer:', isInteger(id_))
+        return
+      };
+ 
+    const hamburguesa = await ctx.orm.hamburguesa.findByPk(id_,{
+      attributes: ['id', 'nombre', 'precio', 'descripcion', 'imagen']
     });
-  });
 
-  //await ctx.render('hamburguesas/index', { hamburguesas });
-});
+      if (hamburguesa) {
+        hamburguesa.destroy();
+        ctx.response.body = "hamburguesa eliminada";
+        ctx.response.status = 200;
+        console.log("200")
 
-module.exports = router;
-*/
+      } else {
+        console.log("404 else")
+        ctx.response.body = "hamburguesa inexistente";
+        ctx.response.status = 404;
+      }
+    })
  
