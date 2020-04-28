640d5167942f1a9bccd5afb023ae4bdc1a4b80ef
joaquineichholz <jeichholz3@uc.cl>
2020-04-28 10:31:42 -0400

    post burger correct

--- src/routes/hamburguesa.js
+++ src/routes/hamburguesa.js
@@ -52,34 +52,69 @@ router.get('hamburguesa', '/', async (ctx) => {
 // POST new hamburguesa
 router.post('hamburguesa', '/', async (ctx) => {
     // Validate request
-    /* if (!req.body.title) {
-        res.status(400).send({
-          message: "Content can not be empty!"
-        });
-        return;
-      }
-    */
       // Create a hamburguesa
       console.log("crear hamburguesa")
+      nombre = ctx.request.body.nombre;
+      descripcion = ctx.request.body.descripcion;
+      precio = ctx.request.body.precio;
+      imagen = ctx.request.body.imagen;
+
+      if (!nombre || !descripcion || !precio || ! imagen) {
+        ctx.response.body = "input invalido";
+        ctx.response.status = 400;
+        return
+      }
+
+      console.log(Object.keys(ctx.request.body))
+      const set = new Set(Object.keys(ctx.request.body));
+
+      if (set.size != 4) {
+        ctx.response.body = "input invalido";
+        ctx.response.status = 400;
+        return
+      }
+
+      var hamburguesas = await ctx.orm.hamburguesa.findAll({
+        attributes: [[Sequelize.fn('max', Sequelize.col('id')), 'max_id']],
+        raw: true,
+      });
+
+      var max_id = hamburguesas[0].max_id 
+
+      if (!hamburguesas[0].max_id) {
+        max_id = 1;
+        console.log('No habian hamburguesas')
+      }
+      else {
+        max_id++ 
+      }
+
+      console.log('\n\n -----------    -------------\n\t', max_id, "\n\n ------------------------")
+
+
       const hamburguesa = {
-        nombre: ctx.request.body.nombre,
-        descripcion: ctx.request.body.descripcion,
-        precio: ctx.request.body.precio,
-        imagen: ctx.request.body.imagen
+        id: max_id,
+        nombre: nombre,
+        descripcion: descripcion,
+        precio: precio,
+        imagen: imagen
       };
       
       console.log("parametros: ", hamburguesa)
     
       // Save hamburguesa in the database
       const new_hamburguesa = ctx.orm.hamburguesa.build(hamburguesa);
-      ctx.assert('new_hamburguesa', 200, 'logrado');
+      new_hamburguesa.save()
+      ctx.response.body = new_hamburguesa;
+      ctx.response.message = 'hamburguesa creada'
+      ctx.response.status = 200;
 
 
     });
 
 module.exports = router;
 
- // GET ONE Hamburger with an id
+ // DELTE ONE Hamburger with an id
  router.del('/hamburguesa', '/:id', async (ctx) => {
   console.log('DELETE ONE')
 
