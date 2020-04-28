a77b4ae203bc1e72a97c1f8aec84025d65cfde7e
joaquineichholz <jeichholz3@uc.cl>
2020-04-27 22:47:30 -0400

    post burger

--- src/models/hamburguesa.js
+++ src/models/hamburguesa.js
@@ -1,6 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const hamburguesa = sequelize.define('hamburguesa', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+  },
     nombre: DataTypes.STRING,
     precio: DataTypes.INTEGER,
     descripcion: DataTypes.STRING,
--- src/routes/hamburguesa.js
+++ src/routes/hamburguesa.js
@@ -1,5 +1,7 @@
 const KoaRouter = require('koa-router');
 
+const Sequelize = require('sequelize');
+
 const router = new KoaRouter();
 
 
@@ -16,6 +18,92 @@ router.get('hamburguesa', '/', async (ctx) => {
     ctx.response.status = 200;
  });
 
+ // GET ONE Hamburger with an id
+ router.get('hamburguesa/:id', '/', async (ctx) => {
+  console.log('GET ONE')
+    if (!ctx.request.body.id) {
+        res.status(400).send({
+          message: "id invalido"
+        });
+        return;
+      }
+
+    const id_ = ctx.request.body.id;
+  
+    const hamburguesa = await ctx.orm.hamburguesa.findByPk({
+      where: {id: id_},
+      attributes: ['id', 'nombre', 'precio', 'descripcion', 'imagen']
+    });
+
+      if (hamburguesa) {
+        ctx.response.body = hamburguesa;
+        ctx.response.status = 200;
+        console.log("200")
+
+      } else {
+        console.log("404 else")
+        ctx.response.body = "hamburguesa inexistente";
+        ctx.response.status = 404;
+      }
+    })
+      /*.catch(err => {
+        console.log("404 error")
+        res.status(404).send({
+          message: "hamburguesa inexistente" // revisar si es un error no encontrar la hamburguesa 
+        });
+      });*/
+
+// POST new Hamburger
+router.post('burgers', '/', async (ctx) => {
+  // Validate request
+  /* if (!req.body.title) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+  */
+    // Create a Hamburger
+    console.log("crear hamburguesa")
+    const hamburguesas = await ctx.orm.hamburguesa.findAll({
+      attributes:  [Sequelize.fn('max', Sequelize.col('id'))],
+      raw: true,
+     });
+    console.log('hamburguesas:',hamburguesas[0].max)
+
+    var id_ = hamburguesas[0].max;
+    if (!id_){
+      console.log(' id:', id_)
+      id_ = 1;
+
+     // console.log('max_id', max_id)
+
+    }
+    else {
+      id_ += 1; 
+      console.log('else', id_)
+
+    }
+    console.log('fuera else', id_)
+
+    const hamburger = {
+      id:id_,
+      nombre: ctx.request.body.nombre,
+      descripcion: ctx.request.body.descripcion,
+      precio: ctx.request.body.precio,
+      imagen: ctx.request.body.imagen
+    };
+
+    
+    console.log("parametros: ", hamburger)
+  
+    // Save Hamburger in the database
+    const new_burger = ctx.orm.hamburguesa.build(hamburger);
+    new_burger.save({fields: ['id', 'nombre', 'precio', 'descripcion', 'foto']})
+    ctx.response.body = new_burger;
+    ctx.response.status = 201;
+
+  });
 
 // POST new hamburguesa
 router.post('hamburguesa', '/', async (ctx) => {
